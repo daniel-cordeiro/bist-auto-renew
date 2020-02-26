a9e4d96a70c47d140ceec7c945e2cc303786f1b0
Daniel Cordeiro <danielcordeiro1995@gmail.com>
2020-02-26 16:04:13 +0000

    real form request info

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ form_data = {'entry.1228452486':'Python',
             'entry.6150132':'python@mail.com'}
 
 headers = {'Referer':'https://docs.google.com/forms/d/e/1FAIpQLSdVwcxOy1qDiEEoNItOvMvwEA851Z0I9uEcxEeWLzLn-6HhSQ/viewform?',
-            'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134"}
+            'User-Agent':'Python BIST-AUTO-RENEW'}
 
 r = requests.post(url, data=form_data, headers=headers)
 
